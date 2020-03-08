731a8d05d62519e23e10e7d5b1346702c99f17f3
cxz5309 <cxz5309@gmail.com>
2020-03-08 14:03:35 +0900

    no message

---
+++ VictoryEffectTutorial/Assets/ButtonScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    public GameObject BoxConfetti;
+    public GameObject ConConfetti;
+
+    public void OnConButttonClick()
+    {
+        GameObject ob = Instantiate(ConConfetti);
+        Destroy(ob, 2.5f);
+    }
+    public void OnBoxButttonClick()
+    {
+        GameObject ob = Instantiate(BoxConfetti);
+        Destroy(ob, 5f);
+    }
+}
